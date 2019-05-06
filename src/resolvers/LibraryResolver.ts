412a2509df12e965a0bc1b91ba7a4bb3d28daa53
Taylor Howard <taylor@taylorrayhoward.com>
2019-05-06 13:09:01 -0600

    Add books and authors to library resolver

--- src/resolvers/LibraryResolver.ts
+++ src/resolvers/LibraryResolver.ts
@@ -1,16 +1,58 @@
-import { Arg, Query, Resolver } from 'type-graphql';
+import { Arg, FieldResolver, Query, Resolver, Root } from 'type-graphql';
 import { LibraryType } from '../types/LibraryType';
 import { Library } from '../entity/Library';
+import { BookType } from '../types/BookType';
+import { Book } from '../entity/Book';
+import { LibraryBook } from '../entity/LibraryBook';
+import { AuthorType } from '../types/AuthorType';
+import { AuthorBook } from '../entity/AuthorBook';
+import { In } from 'typeorm'
+import { Author } from '../entity/Author';
 
 @Resolver(type => LibraryType)
 export class LibraryResolver {
   @Query(returns => [LibraryType])
   async libraries(): Promise<LibraryType[]> {
-    return Library.find();
+    const x = Library.find();
+    return x;
   }
 
   @Query(returns => LibraryType, { nullable: true })
   async library(@Arg('id') id: string): Promise<Library | undefined> {
     return Library.findOne(id);
+  }
+
+  @FieldResolver(returns => [BookType])
+  async books(@Root() library: LibraryType): Promise<Book[]> {
+    const books = await LibraryBook.find({
+      relations: ['book'], where: {
+        libraryId: library.id
+      }
+    });
+
+    return books.map(b => b.book);
+  }
+
+  @FieldResolver(returns => [AuthorType])
+  async authors(@Root() library: LibraryType): Promise<Author[]> {
+    const bookIds = (await LibraryBook.find({
+      where: {
+        libraryId: library.id
+      }
+    })).map(b => b.bookId);
+
+    const authorIds = (await AuthorBook.find({
+      where: {
+        bookId: In(bookIds)
+      }
+    })).map(a => a.authorId);
+
+    const unique = [...new Set(authorIds)];
+
+    return Author.find({
+      where: {
+        id: In(unique)
+      }
+    })
   }
 }
